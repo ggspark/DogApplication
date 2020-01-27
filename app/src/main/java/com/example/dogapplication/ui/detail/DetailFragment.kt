35fcc2bb13f90a954709f1071a83033e1f50550d
Gaurav <gg.spark@gmail.com>
2020-01-27 22:07:08 +0530

    Added detail view with dog

--- app/src/main/java/com/example/dogapplication/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/dogapplication/ui/detail/DetailFragment.kt
@@ -1,33 +1,28 @@
 package com.example.dogapplication.ui.detail
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import com.example.dogapplication.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.dogapplication.databinding.DetailFragmentBinding
 
 class DetailFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = DetailFragment()
-    }
-
-    private lateinit var viewModel: DetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.detail_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
-        // TODO: Use the ViewModel
+        val application = requireNotNull(activity).application
+        val binding = DetailFragmentBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        val dog = DetailFragmentArgs.fromBundle(arguments!!).selectedDog
+        val viewModelFactory = DetailViewModelFactory(dog, application)
+        binding.viewModel = ViewModelProviders.of(
+            this, viewModelFactory
+        ).get(DetailViewModel::class.java)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/dogapplication/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/dogapplication/ui/detail/DetailViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.dogapplication.ui.detail
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.dogapplication.entity.Dog
 
-class DetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class DetailViewModel(dog: Dog, app: Application) : AndroidViewModel(app) {
+    private val _selectedDog = MutableLiveData<Dog>()
+
+    // The external LiveData for the _selectedDog
+    val selectedDog: LiveData<Dog>
+        get() = _selectedDog
+
+    // Initialize the _selectedDog MutableLiveData
+    init {
+        _selectedDog.value = dog
+    }
 }
